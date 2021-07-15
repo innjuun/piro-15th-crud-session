7fa12ce1aa7835ee9fb43d81bec0ab3d53ef8563
innjuun <sroo2315@gmail.com>
2021-07-16 00:22:37 +0900

    second commit

--- club/models.py
+++ club/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    generation = models.IntegerField("기수")
+    birth_date = models.DateField()
+    email = models.EmailField()
+    phone_number = models.CharField("연락처", max_length=15)
+    profile = models.ImageField("프로필 사진", null=True)
+    introduction = models.TextField("자기소개", max_length=1000, null=True)
