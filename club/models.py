e60f31ebec62422c1088507058ec104739703ce1
innjuun <sroo2315@gmail.com>
2021-07-16 01:11:27 +0900

    Link admin page

--- club/models.py
+++ club/models.py
@@ -8,3 +8,6 @@ class Member(models.Model):
     phone_number = models.CharField("연락처", max_length=15)
     profile = models.ImageField("프로필 사진", null=True, upload_to="images/")
     introduction = models.TextField("자기소개", max_length=1000, null=True)
+
+    def __str__(self):
+        return str(self.phone_number)
