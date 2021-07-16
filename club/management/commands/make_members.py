a0eb420728174b364e642b8dcbe002ff5803c911
innjuun <sroo2315@gmail.com>
2021-07-16 22:33:23 +0900

    Fix command

--- club/management/commands/make_members.py
+++ club/management/commands/make_members.py
@@ -1,8 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from club.models import Member
 import faker
-from faker.factory import Factory
-
+import random
 fake = faker.Faker('ko_KR')
 
 class Command(BaseCommand):
@@ -15,12 +14,12 @@ class Command(BaseCommand):
         print(options)
         for _ in range(options['numbers'][0]):
             Member.objects.create(
-                generation=fake.random_digit(),
+                generation=random.randint(1, 15),
                 birth_date=fake.date(),
                 email=fake.email(),
                 phone_number=fake.phone_number(),
                 introduction=fake.paragraph(nb_sentences=3),
-                
+
             )
 
             self.stdout.write(self.style.SUCCESS('Succesfully made members of"%s"' % options['numbers'][0]))
