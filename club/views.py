3e38bfb5718ff8881441f26f806a065deb4da243
innjuun <sroo2315@gmail.com>
2021-07-17 13:15:35 +0900

    Add introduction, redirect button, clean phone number

--- club/views.py
+++ club/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from club.forms import MemberForm
 from club.models import Member
 from django.shortcuts import redirect, render
-
+import re
 # Create your views here.
 
 def member_list(request):
@@ -39,7 +39,11 @@ def member_update(request, pk):
             member.generation = form.cleaned_data["generation"]
             member.birth_date = form.cleaned_data["birth_date"]
             member.email = form.cleaned_data["email"]
-            member.phone_nuumber = form.cleaned_data["phone_number"]
+            cleaned_phone_number = form.cleaned_data["phone_number"]
+            if re.match(r"(\d+){11}", cleaned_phone_number):
+                member.phone_number = f"{cleaned_phone_number[:3]}-{cleaned_phone_number[3:7]}-{cleaned_phone_number[7:11]}"
+            else:
+                member.phone_number = cleaned_phone_number
             member.profile = form.cleaned_data.get("profile")
             member.introduction = form.cleaned_data.get("introduction")
             member.save()
